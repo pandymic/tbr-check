fccad2c2551b22ff5d1b2f6b5da3ca6e69efb654
Michal Pandyra <mike@pandymic.com>
2025-02-16 14:02:27 -0800

    Added dictionary support to API

--- api/public/index.php
+++ api/public/index.php
@@ -8,6 +8,12 @@ $config = include __DIR__ . '/../../etc/config.php';
 $config->now = date( 'Y-m-d H:i:s', $config->time );
 $config->today = date( 'Y-m-d', $config->time );
 
+$config->words = array_values( array_map( function( $word ) {
+  return strtolower( trim( $word ) );
+}, array_filter( preg_split('/[\r\n]+/', file_get_contents( $config->words ) ), function( $word ) {
+  return strlen( trim( $word ) ) > 2;
+} ) ) );
+
 $config->db = new PDO( 'mysql:host=' . $config->db->host . ';dbname=' . $config->db->name, $config->db->user, $config->db->pass );
 $config->db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
@@ -19,14 +25,52 @@ $slim->group( '/api/v1', function( $slim ) use ( $config ) {
     return $response->withStatus( 200 )->getBody()->write( 'Hello world!' );
   } );
 
+  $slim->get( '/words', function( $request, $response, $args ) use ( $config ) {
+    return $response->withJson( $config->words );
+  } );
+
   $slim->get( '/domains', function( $request, $response, $args ) use ( $config ) {
 
-    $selectStmt = $config->db->prepare('SELECT id, domain as name, date_tbr as tbr, date_added as added, date_removed as removed FROM domains WHERE date_tbr >= :today AND date_removed IS NULL ORDER BY date_added DESC, date_tbr ASC, domain ASC');
+    $selectStmt = $config->db->prepare('SELECT id, domain as name, date_tbr as tbr, date_added as added, date_removed as removed FROM domains WHERE date_tbr >= :today AND date_removed IS NULL AND domain NOT REGEXP "[\d-]" AND domain NOT REGEXP ".{10,}" ORDER BY date_added DESC, date_tbr ASC, domain ASC');
     $selectStmt->bindParam(':today', $config->today);
     $selectStmt->execute();
     $domainList = $selectStmt->fetchAll(PDO::FETCH_OBJ);
 
     return $response->withJson( $domainList );
+  } );
+
+  $slim->get( '/domains/{id:[0-9]+}/words', function( $request, $response, $args ) use ( $config ) {
+
+    $domainId = $args['id'];
+
+    $selectStmt = $config->db->prepare('SELECT id, domain as name, date_tbr as tbr, date_added as added, date_removed as removed FROM domains WHERE id = :id');
+    $selectStmt->bindParam(':id', $domainId, PDO::PARAM_INT);
+    $selectStmt->execute();
+    $domain = $selectStmt->fetch(PDO::FETCH_OBJ);
+
+    if ( $domain ) {
+
+      $name = trim( preg_replace( '/\.ca$/', '', trim( strtolower( $domain->name ) ) ) ); // Strip bullshit.
+
+      foreach ( $config->words as $word ) {
+        if ( false !== strpos( $name, $word ) ) {
+          if ( !isset( $domain->words ) ) $domain->words = [];
+          if ( !in_array( $word, $domain->words ) ) $domain->words[] = $word;
+        }
+      }
+      if ( isset( $domain->words ) ) {
+        usort( $domain->words, function( $a, $b ) {
+          return strlen( $b ) - strlen( $a );
+        } );
+      }
+
+      unset( $domain->tbr );
+      unset( $domain->added );
+      unset( $domain->removed );
+
+      return $response->withJson( $domain );
+    }
+    return $response->withStatus(404, 'Domain not found');
   } );
 
   $slim->map( [ 'GET', 'POST', 'PUT', 'DELETE' ], '/domains/{id:[0-9]+}', function( $request, $response, $args ) use ( $config ) {
